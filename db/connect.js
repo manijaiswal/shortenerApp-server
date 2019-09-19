f62ac01d1556ccef1d7ade6e04ff3b38b5b474c7
JAIMAKALI <mk6598951@gmail.com>
2019-09-20 01:08:48 +0530

    deploy

--- db/connect.js
+++ db/connect.js
@@ -4,7 +4,10 @@ const configs       = require('../utility/configs');
 const log4jsLogger      =   require('../loggers/log4js_module');
 
 var logger              =   log4jsLogger.getLogger('DataBase');
-mongoose.connect('mongodb://localhost:27017/'+configs.DB_NAME,{useNewUrlParser:true,useUnifiedTopology: true},(err)=>{
+
+//mongodb://manish:Nitp123@ds135577.mlab.com:35577/shortnerapp
+//'mongodb://localhost:27017/'+configs.DB_NAME
+mongoose.connect('mongodb://manish:Nitp123@ds135577.mlab.com:35577/shortnerapp',{useNewUrlParser:true,useUnifiedTopology: true},(err)=>{
     if(err){
         console.error(err);
         return;
